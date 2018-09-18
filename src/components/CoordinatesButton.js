5228878d865747ca0cd3587d0062c01405acd0f4
ritz-w <ritz.wu@outlook.com>
2018-09-18 11:51:03 +0100

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+
+    findCoordinates = (event) => {
+        const myCoords = []
+        myCoords.push(event.clientX)
+        myCoords.push(event.clientY)
+        return this.props.onReceiveCoordinates(myCoords)
+    }
+
+    render() {
+        return(
+            <button onClick={this.findCoordinates}>Mouse Coordinates</button>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,14 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+    makeDelay = (event) => {
+        event.persist()
+        setTimeout(this.props.onDelayedClick, this.props.delay, event)
+    }
+    render() {
+        return(
+            <button onClick={this.makeDelay}>Delay</button>
+        )
+    }
+}
